3db098403106cd9ff93bcdd279bd92034cc7bab1
Raymomyar <awesomecatstudio@gmail.com>
2023-06-17 14:38:38 -0700

    util update

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
 import child_process from "child_process";
 
-import logger from "./logger.js";
+import * as logger from "./logger.js";
+
+import fs from "fs";
 
 /**
  *
@@ -11,10 +13,20 @@ export async function execAsync(...args) {
     return new Promise((resolve, reject) => {
         let proc = child_process.spawn(...args);
         // good logging
-        logger.info("Calling " + args[0] + " " + args[1].join(" "));
+        logger.info("Calling `" + args[0] + " " + args[1].join(" ") + "`");
         proc.stdout.pipe(process.stdout);
         proc.stderr.pipe(process.stderr);
         process.stdin.pipe(proc.stdin);
         proc.on("exit", resolve);
+    });
+}
+
+// check file exists async
+export function existsAsync(filepath){
+    return new Promise((resolve, reject) => {
+        fs.access(filepath, fs.constants.F_OK, (err) => {
+            if(err) resolve(false);
+            else resolve(true);
+        });
     });
 }
